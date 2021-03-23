db46bfd127f3daa8b3fdba5d6044a20e63ba0167
goDlin <godlin086@gmail.com>
2021-03-23 12:40:50 +0300

    first

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,35 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Twitter from 'twitter-lite'
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+export default async (req, res) => {
+	const consumer_key = process.env.CONSUMER_KEY
+	const consumer_secret = process.env.CONSUMER_SECRET
+	const access_token_key = process.env.ACCESS_TOKEN_KEY
+	const access_token_secret = process.env.ACCESS_TOKEN_SECRET
+
+	const client = new Twitter({ consumer_key, consumer_secret, access_token_key, access_token_secret })
+
+	const data = await client.get('trends/place', { id: 2122265 })// Moscow, 23424936 }) Russia
+	const trends = data[0].trends.slice(0, 5)
+
+	const promises = trends.map(async (trend) => {
+		const search = await client.get('search/tweets', { q: trend.query, lang: 'ru', /*result_type: 'popular',*/ count: 3 })
+		const ids = search.statuses.map(s => s.id_str)
+
+		return { trend, ids }
+	})
+
+	Promise.all(promises).then(values => {
+		//console.log(values)
+		res.status(200).json({ values })
+	})
+
+	const limit = await client.get('application/rate_limit_status', { resources: 'search' })
+	console.log(limit.resources.search)
+
+	//const url = 'https://twitter.com/' + status.user.screen_name + '/status/' + status.id_str
+	//const { html } = await client.get('statuses/oembed', { url })
+
+	//console.log(result)
+
+  	//res.status(200).json({ result })
 }
---
+++ pages/tweets.js
@@ -0,0 +1,24 @@
+import useSwr from 'swr'
+import { Tweet } from 'react-twitter-widgets'
+
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
+export default function Tweets() {
+    const { data, error } = useSwr('/api/hello', fetcher, { revalidateOnFocus: false })
+
+    if (error) return <div>Failed to load</div>
+    if (!data) return <div>Loading...</div>
+
+    return (
+        <div>
+            {data.values.map(d => {
+                return (
+                    <div>
+                        <h1>{d.trend.name}</h1>
+                        {d.ids.map(id => <Tweet options={{ width: "400px"/*, theme: "dark"*/ }} tweetId={id} />)}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
