dcff5ae271d081443f3a7096db2722fe13bde695
goDlin <godlin086@gmail.com>
2022-04-28 20:26:15 +0300

    svg as background

--- pages/new/index.js
+++ pages/new/index.js
@@ -18,7 +18,7 @@ export default function Home() {
             <link rel="icon" href="/favicon.ico" />
         </Head>
 
-        <div className="container mx-auto px-4">
+        <div className="mx-auto px-4">
 
                 <div className="text-4xl font-bold dark:text-pink-600 text-opacity-50">tweetlive</div>
                 <div className="grid grid-cols-1 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 gap-4">
