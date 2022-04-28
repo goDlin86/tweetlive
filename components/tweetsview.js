13aca2a78eb5b86a7790efaeb7bf894a7eaacdc2
goDlin <godlin086@gmail.com>
2022-04-28 19:12:51 +0300

    add twitter logo

--- components/tweetsview.js
+++ components/tweetsview.js
@@ -8,13 +8,16 @@ export default function TweetsView() {
     if (error) return <div>Failed to load</div>
     if (!data) return <div>Loading...</div>
 
+    console.log(data)
+
     return (
         <>
             {data.map(t => {
                 return (
                     <div>
+                        <img src='twitter.svg' />
                         <img src={t.user.profile_image_url_https} />
-                        <h1 className="truncate">{t.user.screen_name}</h1>
+                        <h1 className='truncate'>{t.user.screen_name}</h1>
                         {t.text}
                     </div>
                 )
