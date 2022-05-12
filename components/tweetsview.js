a6d45e6ed94cef54030c632343933b32afc0ce49
goDlin <godlin086@gmail.com>
2022-05-12 17:05:47 +0300

    color heart

--- components/tweetsview.js
+++ components/tweetsview.js
@@ -34,7 +34,7 @@ export default function TweetsView() {
                         </div>
                         <div>
                             <div className='align-middle heart inline-block' />
-                            <div className='align-middle ml-1 inline-block'>{millify(t.favorite_count)}</div>
+                            <div className='align-middle ml-1 inline-block dark:text-rose-600'>{millify(t.favorite_count)}</div>
                         </div>
                     </div>
                 )
