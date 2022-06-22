fd5a13f168bc56e40a0c90f938de67a5b37efb87
goDlin <godlin086@gmail.com>
2022-06-22 17:09:12 +0300

    add comments count

--- components/tweetsview.js
+++ components/tweetsview.js
@@ -36,6 +36,9 @@ export default function TweetsView() {
                             {dayjs(t.created_at).fromNow()}
                         </div>
                         <div>
+                            <div className='align-middle comment inline-block' />
+                            <div className='align-middle ml-1 inline-block dark:text-slate-800'>{millify(t.retweet_count)}</div>
+                            
                             <div className='align-middle heart inline-block' />
                             <div className='align-middle ml-1 inline-block dark:text-rose-600 text-opacity-70'>{millify(t.favorite_count)}</div>
                         </div>
