663c9682b8ae181303fb00db10e22aaed6a2ec5b
Denis <godlin086@gmail.com>
2023-02-13 19:39:53 +0300

    return fix

--- pages/api/inngest.js
+++ pages/api/inngest.js
@@ -3,7 +3,7 @@ import { serve } from 'inngest/next'
 
 const revalidate = createScheduledFunction('Revalidate', '0 */2 * * *', async () => {
   const res = await fetch('https://tweetlive.vercel.app/api/revalidate?secret=' + process.env.TWEETLIVE_TOKEN)
-  return res
+  return await res.json()
 })
 
 export default serve('tweetlive', [ revalidate ])
