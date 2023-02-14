677a840d0472fce12ac47c94520a5e00be92a855
Denis <godlin086@gmail.com>
2023-02-14 17:25:42 +0300

    next revalidate 60

--- components/tweetsview.js
+++ components/tweetsview.js
@@ -4,7 +4,7 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
 
 async function fetchData() {
-    const res = await fetch('https://tweetlive.vercel.app/api/tweets')
+    const res = await fetch('https://tweetlive.vercel.app/api/tweets', { next: { revalidate: 60 }})
     const data = await res.json()
     return data
 }
--- pages/api/inngest.js
+++
@@ -1,9 +0,0 @@
-import { createScheduledFunction } from 'inngest'
-import { serve } from 'inngest/next'
-
-const revalidate = createScheduledFunction('Revalidate', '0 */2 * * *', async () => {
-  const res = await fetch('https://tweetlive.vercel.app/api/revalidate?secret=' + process.env.TWEETLIVE_TOKEN)
-  return await res.json()
-})
-
-export default serve('tweetlive', [ revalidate ])
